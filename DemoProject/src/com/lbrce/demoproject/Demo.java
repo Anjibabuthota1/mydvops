056eb49b7b863fd71890abaca533f51b5d6c3818
gopi-sankar <154255773+gopi-sankar@users.noreply.github.com>
2024-08-22 10:56:33 +0530

    Update Demo.java

--- DemoProject/src/com/lbrce/demoproject/Demo.java
+++ DemoProject/src/com/lbrce/demoproject/Demo.java
@@ -5,4 +5,12 @@ public class Demo {
 	 System.out.println("Hello World");
 	 
  }
+	
+}
+public class Remo {
+ public static void main(String[] args) {
+	 System.out.println("Hello motu");
+	 
+ }
+	
 }
